6175cdf3a12c0c4aa9f9f25b5af2b2ea27d17e95
mahesh <maheshpimple2002@gmail.com>
2024-11-14 11:53:06 +0530

    example/temperature-measurement-app - Resolve buffer issues - Use single buffer for store and retrieve of diagnostics - Resolve data loss issue

--- examples/temperature-measurement-app/esp32/main/diagnostic-logs-provider-delegate-impl.cpp
+++ examples/temperature-measurement-app/esp32/main/diagnostic-logs-provider-delegate-impl.cpp
@@ -29,11 +29,6 @@ using namespace chip::app::Clusters::DiagnosticLogs;
 LogProvider LogProvider::sInstance;
 LogProvider::CrashLogContext LogProvider::sCrashLogContext;
 
-#if CONFIG_ENABLE_ESP_DIAGNOSTICS_TRACE
-    static uint8_t retrieveBuffer[RETRIEVAL_BUFFER_SIZE];
-    MutableByteSpan endUserSupportSpan(retrieveBuffer, sizeof(retrieveBuffer));
-#endif
-
 namespace {
 bool IsValidIntent(IntentEnum intent)
 {
@@ -83,7 +78,7 @@ size_t LogProvider::GetSizeForIntent(IntentEnum intent)
     case IntentEnum::kEndUserSupport:
         {
             #if CONFIG_ENABLE_ESP_DIAGNOSTICS_TRACE
-                return RETRIEVAL_BUFFER_SIZE;
+                return DIAGNOSTIC_BUFFER_SIZE;
             #else
                 return static_cast<size_t>(endUserSupportLogEnd - endUserSupportLogStart);
             #endif
@@ -122,10 +117,10 @@ CHIP_ERROR LogProvider::PrepareLogContextForIntent(LogContext * context, IntentE
     case IntentEnum::kEndUserSupport: {
         #if CONFIG_ENABLE_ESP_DIAGNOSTICS_TRACE
             DiagnosticStorageImpl & diagnosticStorage = DiagnosticStorageImpl::GetInstance();
+            MutableByteSpan endUserSupportSpan(endUserBuffer, DIAGNOSTIC_BUFFER_SIZE);
 
             if (diagnosticStorage.IsEmptyBuffer())
             {
-                printf("Buffer is empty\n");
                 ChipLogError(DeviceLayer, "Empty Diagnostic buffer");
                 return CHIP_ERROR_NOT_FOUND;
             }
@@ -144,6 +139,7 @@ CHIP_ERROR LogProvider::PrepareLogContextForIntent(LogContext * context, IntentE
         #endif
     }
     break;
+
     case IntentEnum::kNetworkDiag: {
         context->NetworkDiag.span =
             ByteSpan(&networkDiagnosticLogStart[0], static_cast<size_t>(networkDiagnosticLogEnd - networkDiagnosticLogStart));
